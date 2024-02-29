dcb434a1c90de4e68424f29a3c1e80a04f468610
Pablo Tanner <15524263+pablotanner@users.noreply.github.com>
2024-02-29 20:10:37 +0100

    Change back, didnt do anything

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -6,7 +6,7 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-  <div className="relative w-full overflow-y-auto max-h-[500px]">
+  <div className="relative w-full overflow-auto max-h-[500px]">
     <table
       ref={ref}
       className={cn("w-full caption-bottom text-sm", className)}
